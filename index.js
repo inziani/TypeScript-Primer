f8a7ff45d5f135b04671f2d13fe150f399d997a7
“inziani” <ValentineRobai.Inziani@gmail.com>
2020-12-26 16:14:48 +0300

    js types

---
+++ index.js
@@ -0,0 +1,26 @@
+let hatPrice = 100;
+console.log(`Hat price: ${hatPrice}`)
+let bootsPrice = 100;
+console.log(`Boots price:${bootsPrice}`)
+
+if (hatPrice === bootsPrice){
+  console.log('Prices are the same');}
+else {
+
+  console.log('Prices are different')
+  }
+
+let totalPrice = hatPrice + bootsPrice;
+console.log(`Total Cost: ${totalPrice}`)
+let myVariable = "Val";
+let secVar = 100
+console.log(`Variable type: ${typeof myVariable}`)
+console.log(`Variable type: ${typeof secVar}`)
+
+let firstCity;
+let secondCity = firstCity || 'London';
+console.log(`City: ${secondCity}`);
+
+function sumPrices(){
+
+}
